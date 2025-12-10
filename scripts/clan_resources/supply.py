2fe79ae28f08d9869c614ee67282f40fab1ccc80
sedgestripe <110428732+sedgestripe@users.noreply.github.com>
2025-12-09 22:39:20 -0600

    test

--- scripts/clan_resources/supply.py
+++
@@ -1,9 +0,0 @@
-from strenum import StrEnum
-
-
-class Supply(StrEnum):
-    EMPTY = "empty"
-    LOW = "low"
-    ADEQUATE = "adequate"
-    FULL = "full"
-    EXCESS = "excess"
--- scripts/game_structure/game/switches/__init__.py
+++
@@ -1,9 +0,0 @@
-from scripts.game_structure.game.switches.game_switches import (
-    Switch,
-    switch_get_value,
-    switch_set_value,
-    switch_append_list_value,
-    switch_remove_list_value,
-    switch_set_dict_value,
-    switch_generator,
-)
