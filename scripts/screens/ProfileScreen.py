236cde7dec20f858d96499f1852beb1f8634cbfc
sedgestripe <110428732+sedgestripe@users.noreply.github.com>
2025-12-30 19:54:51 -0600

    Update ProfileScreen.py

--- scripts/screens/ProfileScreen.py
+++ scripts/screens/ProfileScreen.py
@@ -1040,6 +1040,7 @@ class ProfileScreen(Screens):
                 (not self.the_cat.dead and not self.the_cat.status.is_outsider and game.clan.your_cat.status.is_outsider and not game.clan.your_cat.dead) or 
                 game.clan.your_cat.moons < 0 or
                 self.the_cat.ID == game.clan.your_cat.ID or
+                self.the_cat.status.group_ID in [clan.group_ID for clan in game.clan.all_other_clans] or 
                 ((game.clan.your_cat.dead or self.the_cat.dead) and dead_talk is False)
             ):
                 cant_talk = True
@@ -1063,6 +1064,7 @@ class ProfileScreen(Screens):
                 (not self.the_cat.dead and not self.the_cat.status.is_outsider and game.clan.your_cat.status.is_outsider and not game.clan.your_cat.dead) or 
                 game.clan.your_cat.moons < 0 or
                 self.the_cat.ID == game.clan.your_cat.ID or
+                self.the_cat.status.group_ID in [clan.group_ID for clan in game.clan.all_other_clans] or 
                 (game.clan.your_cat.dead is True or self.the_cat.dead is True and
                 dead_talk is False) or
 
