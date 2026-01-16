031758fd8d8beb1ece8ec26f77a46b1c578f7767
Jay <jayolesonbz@gmail.com>
2026-01-15 22:31:12 -0600

    alive_in_your_cat_group clan check

--- scripts/cat/status.py
+++ scripts/cat/status.py
@@ -325,7 +325,12 @@ class Status:
         """
         Returns True if the cat is currently part of the same group as your cat
         """
-        return self.group == game.clan.your_cat.status.group
+        # this fails tests bc it checks this before Clan exists
+        # so... nonecheck failsafe
+        if game.clan:
+            return self.group == game.clan.your_cat.status.group
+        else:
+            return self.alive_in_player_clan
 
     @property
     def is_outsider(self) -> bool:
