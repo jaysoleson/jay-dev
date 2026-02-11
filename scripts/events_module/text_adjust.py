667f0ab7a3da26b9c266b4711b8526e354d016f6
Jay <jayolesonbz@gmail.com>
2026-02-11 16:15:53 -0600

    more traits added to schema

--- scripts/events_module/text_adjust.py
+++ scripts/events_module/text_adjust.py
@@ -57,25 +57,29 @@ def pronoun_repl(m, cat_pronouns_dict, raise_exception=False):
     # if the cat that the pronoun is assigned to wasn't passed with the dict, then we just return
     # it's assumed that the text is going to be processed at some other point with that cat's info
     # (for example, this is required for rel log processing to be done correctly)
-    if inner_details[1] != "PLURAL" and inner_details[1] not in cat_pronouns_dict:
+
+    # LG EDIT
+    abbrev_check = inner_details[1]
+
+    # LG
+    # addon pronoun test stuff
+    # grabbing the real abbrev from inside of the addons
+    if raise_exception:
+        if "-" in abbrev_check:
+            fragments = abbrev_check.split("-")
+            for f in fragments:
+                if "_" in f or f in ["theircrush", "yourcrush"]:
+                    abbrev_check = f
+                    break
+    # ---
+    if abbrev_check != "PLURAL" and abbrev_check not in cat_pronouns_dict:
         return m.group(0)
 
     try:
-        # LG
-        # addon pronoun test stuff
-        if raise_exception:
-            abbrev = inner_details[1]
-            if "-" in abbrev:
-                fragments = abbrev.split("-")
-                for f in fragments:
-                    if "_" in f or f in ["theircrush", "yourcrush"]:
-                        inner_details[1] = f
-                        break
-        # ---
-        if inner_details[1].upper() == "PLURAL":
+        if abbrev_check.upper() == "PLURAL":
             inner_details.pop(1)  # remove plural tag so it can be processed as normal
             catlist = []
-            for cat in inner_details[1].split("+"):
+            for cat in abbrev_check.split("+"):
                 try:
                     catlist.append(cat_pronouns_dict[cat][1])
                 except KeyError as e:
