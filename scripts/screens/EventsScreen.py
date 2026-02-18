66111fec14450f91223671fdeeaa96a325447de9
sedgestripe <110428732+sedgestripe@users.noreply.github.com>
2026-02-18 13:53:17 -0600

    Update EventsScreen.py

--- scripts/screens/EventsScreen.py
+++ scripts/screens/EventsScreen.py
@@ -617,7 +617,7 @@ class EventsScreen(Screens):
         if game.clan.your_cat.status.is_outsider:
             self.update_heading_text("Your Territory")
         else:
-            self.update_heading_text(self.clan_name)
+            self.update_heading_text(game.clan.name)
         self.show_menu_buttons()
 
     def display_change_save(self) -> Dict:
