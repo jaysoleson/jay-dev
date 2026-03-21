e64ba936bd2556206891f95b88069c95db919817
Jay <jayolesonbz@gmail.com>
2026-03-21 16:31:56 -0500

    event reformatting fixes

--- scripts/events_module/reformat_lifegen_events.py
+++ scripts/events_module/reformat_lifegen_events.py
@@ -1,5 +1,6 @@
 import json
 import copy
+import re
 
 
 file_names = [
@@ -132,6 +133,21 @@ clusters = [
 with open("scripts/events_module/dialogue/reformat_resources/abbrev_convert.json", 'r') as read_file:
     abbrev_convert = json.loads(read_file.read())
 
+with open("scripts/events_module/dialogue/reformat_resources/all_resources.json", 'r') as read_file:
+    resources = json.loads(read_file.read())
+
+# assign variables from resources
+cluster_addons = resources["cluster_addons"]
+skill_addons = resources["skill_addons"]
+
+def get_matches(abbrev, text):
+    match = re.search(rf'(?:([A-Za-z]+)-)?{re.escape(abbrev)}(?:-([A-Za-z]+))?', text)
+    full_abbrev = match.group(0)
+    rel_addon = match.group(1)
+    cluster_addon = match.group(2)
+
+    return full_abbrev, rel_addon, cluster_addon
+
 for folder in folder_names:
     for status in file_names:
         old_events = {}
@@ -177,10 +193,36 @@ for folder in folder_names:
                 check = copy.deepcopy(abbrev_convert)
                 for abbrev, abbrev_info in check.items():
                     if abbrev in event_string:
+                        full_abbrev, rel, cluster = get_matches(abbrev, event_string)
                         # get new r_c abbrev
                         new_abbrev = f"r_c:{random_cat_count}"
                         random_cat_count += 1
-                        event_string = event_string.replace(abbrev, new_abbrev)
+                        event_string = event_string.replace(full_abbrev, new_abbrev)
+
+                        rel_list = []
+                        cluster_list = []
+                        skill_list = []
+
+                        if rel:
+                            rel_dict = {
+                                "dislike": "max_like_-15",
+                                "plike": "min_like_20",
+                                "plove": "min_like_50",
+                                "rlike": "min_romance_20",
+                                "rlove": "min_romance_50",
+                                "trust": "min_trust_20",
+                                "jealous": "max_respect_-10",
+                                "jealousy": "max_respect_-10",
+                                "comfort": "min_comfort_15"
+                            }
+                            if rel in rel_dict:
+                                rel_list.append(rel_dict[rel])
+                        if cluster:
+                            if cluster in cluster_addons:
+                                cluster_list.append(cluster)
+                            elif cluster in skill_addons:
+                                skill_list.append(cluster)
+
 
                         if "relationship" in abbrev_info.copy():
                             for item in abbrev_info["relationship"].copy():
@@ -194,6 +236,27 @@ for folder in folder_names:
                             rel_block.append(abbrev_info["relationship"])
                             abbrev_info.pop("relationship")
                         cats_block[new_abbrev] = abbrev_info
+
+                        if cluster_list:
+                            cats_block[new_abbrev]["cluster"] = cluster_list
+                        if skill_list:
+                            cats_block[new_abbrev]["skill"] = skill_list
+                        if rel_list:
+                            print("Rel list present for", full_abbrev)
+                            found = False
+                            for item in rel_block:
+                                if new_abbrev in item["cats_to"] and "y_c" in item["cats_from"]:
+                                    found = True
+                                    item["relationship"].extend(rel_list)
+                            if not found:
+                                rel_block.append( 
+                                    {
+                                        "cats_from": ["y_c"],
+                                        "cats_to": [new_abbrev],
+                                        "relationship": rel_list
+                                    }
+                                )
+                                
 
                 # get key and block for final writing
                 new_event_key = key.split(" ")[0] + str(event_count)
